58f09047883d6022197f5d87ac9426356e9068e6
Ian <152772a@mymail.nyp.edu.sg>
2019-02-26 15:59:40 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+
+//    printf("hello");
+    int x = 100, y = 2, z;
+    int largest = 0;
+    scanf("d", x);
+    printf("The product of %d and %d is %d\n", x, y);
+    z= x + y;
+    if (x > largest)
+        x == largest;
+    scanf("%d", &largest);
+    printf("The remainder of %d divided by %d is\n", x, y, x % y);
+    if (x = y);
+    printf("%d is equal to %d\n", x, y);
+    printf("The sum is %d\n", x + y);
+    printf("The value of x is %d\n", &x);
+    return 0;
+
+
+}
